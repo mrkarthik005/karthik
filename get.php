eab2b6d5dea32f12755bf774ce31347e204f494d
karthik <70352515+mrkarthik005@users.noreply.github.com>
2021-02-19 15:48:21 +0530

    Add files via upload

---
+++ get.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br><br>
+Your contact number is: <?php echo $_GET["number"]; ?><br><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
